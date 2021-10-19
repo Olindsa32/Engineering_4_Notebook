97d06791eeeece3140b38bacec93cb4d274992f9
Owen Lindsay <61475474+Olindsa32@users.noreply.github.com>
2021-10-19 11:54:17 -0400

    Update Python_Strings_and_Loops.py

--- Python/Python_Strings_and_Loops.py
+++ Python/Python_Strings_and_Loops.py
@@ -1,10 +1,10 @@
 # Python Strings and Loops
 # Written by Owen Lindsay
 # 9/28/21
-def splitWords(word):
-    return list(word)
-sentence = input('Type in your text, then press Enter:')
-for word in sentence.split():
-    for character in splitWords(word):
+def splitWords(word):   # Creates a function to split each word into a list of the letters
+    return list(word)   # The actual command to split the words; I will use this function later after I split the sentence
+sentence = input('Type in your text, then press Enter:')    # Asks a question so that you can respond with a random sentence to work on
+for word in sentence.split():   # Splits the sentence into individual words
+    for character in splitWords(word):  # this line and the line below are calling the function to split the words then printing each character in a seperate line
         print(character)
-    print('-')
+    print('-')  # This puts the dash in before repeating the whole thing to print the next word
