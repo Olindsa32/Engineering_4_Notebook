d8844ac394934b335afe98ec5dc002c38d4114cd
Owen Lindsay <61475474+Olindsa32@users.noreply.github.com>
2021-12-16 11:59:46 -0500

    Update Python_I2C.py

--- Python/Python_I2C.py
+++ Python/Python_I2C.py
@@ -15,3 +15,24 @@ disp = Adafruit_SSM1306.SSD1306_128_64(rst=RST, i2c_address=0x3D)
 disp.begin()
 disp.clear()
 disp.display()
+
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+draw = ImageDraw.Draw(image)
+padding = 2
+shape_width = 20
+top = padding
+bottom = height-padding
+x = padding
+font = ImageFont.load_default()
+
+while True:
+  draw.rectangle((0,0,width,height), outline=0, fill=0)
+  accel, mag = lsm303.read()
+  accel_x, accel_y, accel_z = accel
+  draw.text((x, top),    "x: " + str(round(accel_x/107, 3)),  font=font, fill=255)
+  draw.text((x, top+20), "y: " + str(round(accel_y/107, 3)), font=font, fill=255)
+  draw.text((x, top+40), "z: "  + str(round(accel_z/107, 3)), font=font, fill=255)
+  disp.image(image)
+  disp.display()
