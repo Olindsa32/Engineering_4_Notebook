55a2f9309a79ee05bd71fa74e75e0a6d5e74f991
Owen Lindsay <61475474+Olindsa32@users.noreply.github.com>
2022-01-25 11:53:30 -0500

    Create Headless_Accelerometer.py

---
+++ Python/Headless_Accelerometer.py
@@ -0,0 +1,17 @@
+import time
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+import Adafruit_LSM303
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+RST = 24
+lsm303 = Adafruit_LSM303.LSM303()
+disp = Adafruit_SSM1306.SSD1306_128_64(rst=RST, i2c_address=0x3D)
+
+disp.begin()
+disp.clear()
+disp.display()
