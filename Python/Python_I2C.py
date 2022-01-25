c5b2dfe1de45bf06f0112536d1cbdae0dfb07477
olindsa32 <olindsa32@charlottesvilleschools.org>
2022-01-25 16:49:35 +0000

    added updated I2C and headless accelerometer template

--- Python/Python_I2C.py
+++ Python/Python_I2C.py
@@ -10,7 +10,7 @@ from PIL import ImageFont
 
 RST = 24
 lsm303 = Adafruit_LSM303.LSM303()
-disp = Adafruit_SSM1306.SSD1306_128_64(rst=RST, i2c_address=0x3D)
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3D)
 
 disp.begin()
 disp.clear()
